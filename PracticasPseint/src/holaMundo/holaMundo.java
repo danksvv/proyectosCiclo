5da1c03b0c908e2a314bcf5a691cb01c523b8f0b
danksvv <74599036+danksvv@users.noreply.github.com>
2021-10-26 18:42:39 +0200

    Agregado dos lineas de saludo

--- PracticasPseint/src/holaMundo/holaMundo.java
+++ PracticasPseint/src/holaMundo/holaMundo.java
@@ -3,5 +3,7 @@ package holaMundo;
 public class holaMundo {
     public static void main(String[] args) {
         System.out.println("Hola desde Mundo Java, desde Intellij");
+        System.out.println("Hola desde Mundo Java, desde Intellij");
+
     }
 }
