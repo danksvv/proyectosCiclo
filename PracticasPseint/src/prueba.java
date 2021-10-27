72283b2014c9cab972ffa3a548f0f6badfbef4a1
danksvv <74599036+danksvv@users.noreply.github.com>
2021-10-27 20:03:36 +0200

    Prueba de caso switch sin break

---
+++ PracticasPseint/src/prueba.java
@@ -0,0 +1,14 @@
+public class prueba {
+    public static void main(String[] args) {
+        int n=0, x=1;
+
+        switch (x){
+            case 1: n++;
+            case 2: n++;
+            case 3: n++;
+            default: n++;
+        }
+        System.out.println("N:" + n);
+
+    }
+}
